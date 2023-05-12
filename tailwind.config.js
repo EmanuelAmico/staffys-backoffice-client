485ce452bfaa6018f3d58003f8611a001e67d2eb
Rodriix99 <roescal347@gmail.com>
2023-05-12 10:19:23 -0300

    successful tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,17 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        whiteText: "#FFFFFF",
+        primaryBlue: "#217BCE",
+        blackText: "#000000",
+        greyText: "#4F4F4F",
+        backgroundButton: "#E0E0E0",
+        redTrashIcon: "#FA0206",
+        yellowText: "#FCBC12",
+        greenText: "#96DB76",
+        yellowBackground: "#FCBC11",
+        salmonText: "#FF6B6B",
       },
     },
   },

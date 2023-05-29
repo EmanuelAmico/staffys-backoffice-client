e38976765ac07248091a38a22935c7ff31b419f7
Rodriix99 <roescal347@gmail.com>
2023-05-29 15:05:01 -0300

    switch button done

---
+++ src/commons/SwitchButton.tsx
@@ -0,0 +1,35 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import "../styles/SwitchButton.css";
+
+export interface ISwitchButton {
+  isSwitched: boolean;
+  onClick: () => void;
+}
+const SwitchButton: React.FC<ISwitchButton> = ({ isSwitched, onClick }) => {
+  const [railAnimation, setRailAnimation] = useState<string>(
+    "rail-animation-disable"
+  );
+  const [circleAnimation, setCirlceAnimation] = useState<string>(
+    "circle-animation-disable"
+  );
+
+  useEffect(() => {
+    if (isSwitched) {
+      setRailAnimation("rail-animation-active");
+      setCirlceAnimation("circle-animation-active");
+    } else {
+      setRailAnimation("rail-animation-disable");
+      setCirlceAnimation("circle-animation-disable");
+    }
+  }, [isSwitched]);
+
+  return (
+    <button onClick={onClick} className="button">
+      <div className={`rail ${railAnimation}`}></div>
+      <div className={`circle ${circleAnimation}`}></div>
+    </button>
+  );
+};
+
+export default SwitchButton;

91b0745aa461a397825c312b5d5757dd82bd8dec
Rodriix99 <roescal347@gmail.com>
2023-05-29 17:44:26 -0300

    done

--- src/commons/PackageTransportCard.tsx
+++ src/commons/PackageTransportCard.tsx
@@ -23,8 +23,7 @@ const PackageTransportCard: FC<PackageTransportCardProps> = ({
       <div className="flex flex-col gap-1 ml-2">
         <p className="font-bold text-base">{transporterName}</p>
         <p className="font-medium text-sm text-primaryBlue flex items-center">
-          <span className="mr-1 h-2 w-2 rounded-full bg-primaryBlue"/>{" "}
-          {status}
+          <span className="mr-1 h-2 w-2 rounded-full bg-primaryBlue" /> {status}
         </p>
       </div>
       <div className="flex items-center justify-center  w-[44px] ml-4">

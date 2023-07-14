32a979bbafe36f59e36277df28c14b41f7ce0fef
Santiago Estevez <elbolaestevez@gmail.com>
2023-07-14 14:24:25 -0300

    login reducer and dispatch done

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,57 @@
-import React from "react";
+import React, { FormEvent } from "react";
 import Layout from "@/commons/Layout";
 import Button from "@/commons/Button";
 import TextInput from "@/commons/TextInput";
 import Image from "next/image";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "@/redux/store";
+import { login } from "@/redux/reducers/selectedUser";
+import useInput from "@/hooks/useInput";
+import { useRouter } from "next/navigation";
 
 const Login = () => {
+  const { push } = useRouter();
+  const dispatch = useDispatch<AppDispatch>();
+  const email = useInput({
+    validators: [
+      {
+        type: "notEmpty",
+        errorMessage: "El email es requerido",
+      },
+      {
+        type: "email",
+        errorMessage: "El email tiene un formato incorrecto",
+      },
+    ],
+  });
+
+  const password = useInput({
+    validators: [
+      {
+        type: "notEmpty",
+        errorMessage: "La contraseña es requerida",
+      },
+      {
+        type: "password",
+        errorMessage: "Debe tener al menos 8 caracteres, una letra y un número",
+      },
+    ],
+  });
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const userData = {
+      email: email.value,
+      password: password.value,
+    };
+    try {
+      await dispatch(login(userData)).unwrap();
+      push("/home");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <Layout className="h-screen">
       <div className="flex justify-center items-end h-[30%]">
@@ -16,7 +63,7 @@ const Login = () => {
           priority
         />
       </div>
-      <form autoComplete="off" className="pt-16 pb-5">
+      <form autoComplete="off" className="pt-16 pb-5" onSubmit={handleSubmit}>
         <TextInput
           label="Usuario"
           name="email"

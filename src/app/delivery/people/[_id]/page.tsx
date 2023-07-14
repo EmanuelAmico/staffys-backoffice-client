c9b2fb4362b0d7980701522a4cb3f29c2fd488cf
Rodriix99 <roescal347@gmail.com>
2023-07-13 22:06:01 -0300

    view

--- src/app/delivery/people/[_id]/page.tsx
+++ src/app/delivery/people/[_id]/page.tsx
@@ -1,9 +1,18 @@
 "use client";
-import React, { useCallback, useEffect } from "react";
-import { useParams } from "next/navigation";
+import React, { useCallback, useEffect, useContext } from "react";
+import { useParams, useRouter } from "next/navigation";
 import { useDispatch } from "react-redux";
 import { setSelectedUser } from "@/redux/reducers/selectedUser";
 import { AppDispatch } from "@/redux/store";
+import DeliveryCollapsibleBox from "@/components/DeliveryCollapsibleBox";
+import Layout from "@/commons/Layout";
+import { deliveryHistory } from "@/utils/FakeDataDeliveryHistory";
+import { deliveryPending } from "@/utils/FakeDataDeliveryPending";
+import IconButton from "@/commons/IconButton";
+import { RiArrowLeftSLine } from "react-icons/ri";
+import DeliveryManProfile from "@/commons/DeliveryManProfile";
+import { CheckRefreshContext } from "@/context/refresh";
+
 function DeliveryPeople() {
   const { _id } = useParams();
   const dispatch = useDispatch<AppDispatch>();
@@ -19,7 +28,35 @@ function DeliveryPeople() {
   useEffect(() => {
     getUser();
   }, [getUser]);
-  return <div>page</div>;
+  const router = useRouter();
+  const { isRefreshed } = useContext(CheckRefreshContext);
+
+  return (
+    <Layout className="gap-4">
+      <IconButton
+        onClick={() => (isRefreshed ? router.push("/home") : router.back())}
+        icon={<RiArrowLeftSLine size={40} />}
+        className="self-start"
+      />
+      <DeliveryManProfile
+        transporterName="Farid"
+        profileImage="/svg/faridProfilePicture.svg"
+      />
+      <DeliveryCollapsibleBox
+        title="Repartos pendientes"
+        description="No tenes 6 repartos pendientes"
+        delivery={false}
+        packages={deliveryPending}
+      />
+      <DeliveryCollapsibleBox
+        title="Historial de repartos"
+        description="Ya repartiste 6 paquetes"
+        delivery={false}
+        packages={deliveryHistory}
+        pathButton="/delivery-history"
+      />
+    </Layout>
+  );
 }
 
 export default DeliveryPeople;

59698dcf05e4fa3a938b088e9604c3fec4556b20
Rafa Mojica <rafael.mojica27@gmail.com>
2023-07-17 16:11:28 -0500

    endpoint conection get available packages

--- src/app/package/manage/page.tsx
+++ src/app/package/manage/page.tsx
@@ -2,17 +2,38 @@
 
 import IconButton from "@/commons/IconButton";
 import Layout from "@/commons/Layout";
-import React, { useContext } from "react";
+import React, { useCallback, useContext, useEffect } from "react";
 import { RiArrowLeftSLine } from "react-icons/ri";
 import { useRouter } from "next/navigation";
 import DeliveryPackageCard from "@/commons/DeliveryPackageCard";
 import { AiFillPlusCircle } from "react-icons/ai";
-import { deliveryPackageData } from "@/utils/FakeDataPackage";
+
 import { CheckRefreshContext } from "@/context/refresh";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "@/redux/store";
+import { showToast } from "@/utils/toast";
+import { getAvailablePackages } from "@/redux/reducers/package";
 
 const ManagePackages = () => {
   const router = useRouter();
   const { changeRefresh, isRefreshed } = useContext(CheckRefreshContext);
+  const dispatch = useDispatch<AppDispatch>();
+  const availablePackages = useSelector(
+    (state: RootState) => state.package.availablePackages
+  );
+
+  const retrieveAvailablePackages = useCallback(async () => {
+    try {
+      await dispatch(getAvailablePackages()).unwrap();
+    } catch (error) {
+      console.error(error);
+      showToast("error", "Error al obtener los paquetes");
+    }
+  }, [dispatch]);
+
+  useEffect(() => {
+    retrieveAvailablePackages();
+  }, [retrieveAvailablePackages]);
 
   return (
     <Layout>
@@ -24,12 +45,17 @@ const ManagePackages = () => {
       <h4 className="mt-4 font-bold text-xl">Paquetes</h4>
       <p className="mb-4">Hay 6 paquetes</p>
       <div className="pt-4 mb-4 px-4 border-t-2 overflow-y-scroll">
-        {deliveryPackageData.map((deliveryPackage) => (
-          <div key={deliveryPackage.id}>
-            <DeliveryPackageCard className="mb-4" {...deliveryPackage} />
-            {deliveryPackage !== deliveryPackageData.at(-1) && (
-              <hr className="mb-4" />
-            )}
+        {availablePackages?.map((_package) => (
+          <div key={_package._id}>
+            <DeliveryPackageCard
+              className="mb-4"
+              address={_package.address}
+              receptorName={_package.receptorName}
+              buttonText=""
+              city={_package.city}
+              trash={true}
+            />
+            {_package !== availablePackages.at(-1) && <hr className="mb-4" />}
           </div>
         ))}
       </div>
